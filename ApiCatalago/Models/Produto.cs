779e84201651ef288661c7c0401dd2a4d239b30d
Kaio Santos <tidesenvolvimento9@amvox.com.br>
2026-03-27 15:59:06 -0300

    commit inicial

---
+++ ApiCatalago/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiCatalago.Models;
+
+public class Produto
+{
+    public int ProtudoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
