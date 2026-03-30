b94b240ee14dedbc4bb98cb395bf2ebcab148044
kaiovinic <tidesenvolvimento9@amvox.com.br>
2026-03-29 22:06:13 -0300

    Removendo arquivos desnecessários (.vs)

---
+++ ApiCatalago/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalago.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalago.Context;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+            
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produto { get; set; }
+}
