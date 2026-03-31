1118e87e4164958e4f3d35d5bf9fb6fa457d3d42
Kaio Santos <tidesenvolvimento9@amvox.com.br>
2026-03-31 16:49:21 -0300

    Aplicando Migrations

--- ApiCatalago/Models/Produto.cs
+++ ApiCatalago/Models/Produto.cs
@@ -2,11 +2,15 @@
 
 public class Produto
 {
-    public int ProtudoId { get; set; }
+    public int ProdutoId { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
     public decimal Preco { get; set; }
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+
+    public  int? CategoriaId { get; set; }
+
+    public Categoria? Categoria { get; set; }
 }
