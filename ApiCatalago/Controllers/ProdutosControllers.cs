38ed29696c88caea27729276e133e835bc64867e
Kaio Santos <tidesenvolvimento9@amvox.com.br>
2026-04-02 13:40:48 -0300

    Metodo Delete

--- ApiCatalago/Controllers/ProdutosControllers.cs
+++ ApiCatalago/Controllers/ProdutosControllers.cs
@@ -67,5 +67,23 @@ namespace ApiCatalago.Controllers
             return Ok(produto);
         }
 
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto Não localizado...");
+
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+        }
+
     }
 }
