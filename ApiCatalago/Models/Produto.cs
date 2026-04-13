aeb2bc8bc67b2de249b24e9f3d5d1a73b78f656a
Kaio Santos <tidesenvolvimento9@amvox.com.br>
2026-04-13 16:37:48 -0300

    Validação personalizada

--- ApiCatalago/Models/Produto.cs
+++ ApiCatalago/Models/Produto.cs
@@ -1,19 +1,21 @@
 ﻿namespace ApiCatalago.Models;
 
+using ApiCatalago.Validations;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
-    [Required]
+    [Required(ErrorMessage = "O campo Nome é obrigatório")]
     [StringLength(80)]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
-    [Required]
-    [StringLength(300)]
+    [Required(ErrorMessage = "O campo Descricao é obrigatório")]
+    [StringLength(300, ErrorMessage = "O campo Descricao deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
     [Required]
     [Column(TypeName = "decimal(10,2")]
@@ -27,4 +29,21 @@ public class Produto
     public int? CategoriaId { get; set; }
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra deve ser maiúscula", new[] { nameof(Nome) });
+            }
+        }
+
+        if (this.Estoque <= 0)
+        {
+            yield return new ValidationResult("O preço deve ser maior que zero", new[] { nameof(this.Estoque) });
+        }
+    }
 }
