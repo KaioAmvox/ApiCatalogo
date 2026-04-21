a2fbca73b683e9efbcc3842a8884b2490c1e5d48
kaiovinic <tidesenvolvimento9@amvox.com.br>
2026-04-20 22:55:21 -0300

    Repositories

--- ApiCatalago/Extensions/ApiExceptionMiddlewareExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ApiCatalago.Extensions
-{
-    public static class ApiExceptionMiddlewareExtensions
-    {
-    }
-}
--- ApiCatalago/Logging/CustomLoggerProviderConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiCatalago.Logging
-{
-    public class CustomLoggerProviderConfiguration
-    {
-        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
-        public int EventID { get; set; } = 0;
-    }
-}
